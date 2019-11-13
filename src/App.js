8fec9551b7b456e1f95b96bc53539b0e89ee84de
vale75020 <valentinosuma@hotmail.com>
2019-11-13 16:18:27 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from "react";
-import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import "./App.css";
+import axios from "axios";
 
 export default class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/users")
+  //     .then(res => console.log(res.data));
+  // }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
         {/* <Navbar title="Github Finder" icon="fab fa-github" /> */}
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import UserItem from './UserItem';
+import React, { Component } from "react";
+import UserItem from "./UserItem";
 
 export class Users extends Component {
-    state = {
-        users: [
-            {id: "1",
-            login: "mojombo",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo"},
-            {id: "2",
-            login: "frank",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo"},
-            {id: "3",
-            login: "john",
-            avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-            html_url: "https://github.com/mojombo"}
-        ]
-    }
-    render() {
-        return (
-            <div style={userStyle}>
-                {this.state.users.map(user => (
-                    // <div key={user.id}>{user.login}</div>
-                    <UserItem key={user.id} user={user}/>
 
-                ))}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.props.users.map(user => (
+          // <div key={user.id}>{user.login}</div>
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
 }
 
-const userStyle ={
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gridGap: '1rem'
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem"
+};
 
-}
+export default Users;
 
-export default Users
+//   state = {
+//     users: [
+//       {
+//         login: "mojombo",
+//         id: 1,
+//         avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+//         html_url: "https://github.com/mojombo"
+//       },
+//       {
+//         login: "defunkt",
+//         id: 2,
+//         avatar_url: "https://avatars0.githubusercontent.com/u/2?v=4",
+//         html_url: "https://github.com/defunkt"
+//       },
+//       {
+//         login: "pjhyett",
+//         id: 3,
+//         avatar_url: "https://avatars0.githubusercontent.com/u/3?v=4",
+//         html_url: "https://github.com/pjhyett"
+//       }
+//     ]
+//  };
