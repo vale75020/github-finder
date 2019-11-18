52dcc5c9cbde54a59436842fda7f894535d0f162
vale75020 <valentinosuma@hotmail.com>
2019-11-18 15:47:17 +0100

    repos

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ export default class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   };
 
   async componentDidMount() {
@@ -46,6 +47,16 @@ export default class App extends Component {
     this.setState({ user: res.data, loading: false });
   }
 
+  // Get users repos
+  getUserRepos = async (username) => { // username => login
+    this.setState({ loading: true });
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ repos: res.data, loading: false });
+  }
+ 
+
   // Clear Users from state
 
   clearUsers = () => {
@@ -60,7 +71,7 @@ export default class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, loading, repos } = this.state;
     return (
       <Router>
         <div className="App">
@@ -81,7 +92,7 @@ export default class App extends Component {
               )} />
               <Route exact path="/about" component={About} />
               <Route exact path ='/user/:login' render={props => (
-                <User {...props} getUser={this.getUser} user={user} />
+                <User {...props} getUser={this.getUser} user={user} getUserRepos={this.getUserRepos} repos={repos} />
               )} />
             </Switch>
           </div>
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
+import React, { Fragment, Component } from "react";
+import Spinner from "../layout/Spinner";
+import Repos from '../repos/Repos'
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login); // the same login of the item i clicked
+    this.props.getUserRepos(this.props.match.params.login); 
   }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired
+ };
 
   render() {
     const {
       name,
-      avatar,
+      company,
+      avatar_url,
       url,
       location,
       bio,
@@ -19,11 +33,77 @@ export default class User extends Component {
       following,
       public_repos,
       public_gists,
-      hireble
+      hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
-    return <div>{name}</div>;
+    if (loading) return <Spinner />;
+    return (
+      <Fragment>
+        <Link to="/" className="btn btn-ligth">
+          Back to search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              style={{ width: "150px" }}
+              alt=""
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className="btn btn-dark my-1">
+              Visite Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
+        </div>
+        <Repos repos={repos} />
+      </Fragment>
+    );
   }
 }
